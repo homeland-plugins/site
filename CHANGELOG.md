4b9f88bf865812bcd2c526eb2f3a5a7d1a1bf448
Jason Lee <huacnlee@gmail.com>
2018-04-18 17:35:04 +0800

    Bootstrap v4, remove simple_form

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+0.2.0
+------
+
+- HTML 改用 Bootstrap V4 的格式，去掉  simple_form;
+
 0.1.2
 -----
 
--- lib/homeland/site/version.rb
+++ lib/homeland/site/version.rb
@@ -1,6 +1,6 @@
 module Homeland
   module Site
-    VERSION     = '0.1.2'
+    VERSION     = '0.2.0'
     NAME        = 'site'
     DESCRIPTION = 'Site for Homeland.'
   end
